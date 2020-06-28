69952535fb31db1cfedd05601ff8fb8c8be138d6
Eder Sena <eder.sena@live.com>
2020-06-28 20:01:00 -0300

    Update desafio_1.js

--- funcoes/desafio_1.js
+++ funcoes/desafio_1.js
@@ -1,39 +1,49 @@
 // somar(3)(4)(5)
-function somar(a) {
-    return function(b) {
-        return function(c) {
-            return a + b + c
+
+function somar(num1) {
+    return function(num2){
+        return function(num3) {
+            return num1 + num2 + num3
         }
     }
 }
 
-const somarAB = somar(3)(4)
-console.log(somarAB(13))
-console.log(somar(13)(20)(30))
+const somarPrimeiro = somar(2)
+const somarSegundo = somarPrimeiro(2)
+console.log(somarSegundo(2))
 
+console.log(somar(1)(1)(1))
 
-
-// fn -> 3 * 7
-// fn -> 3 + 7
-// fn -> 3 - 7
 // calcular(3)(7)(fn)
-function calcular(x) {
-    return function(y) {
-        return function(fn) {
-            return fn(x, y)
+function calcular(arg1){
+    return function(arg2){
+        return function(fn){
+            return fn(arg1,arg2)
         }
     }
 }
 
-function subtrair(a, b) {
-    return a - b
+const calc = calcular(10)(2)
+function mult(arg1,arg2){
+    return arg1 * arg2
 }
 
-function multiplicar(a, b) {
-    return a * b
+
+function div(arg1,arg2){
+    return arg1 / arg2
 }
 
-const r1 = calcular(10)(5)(subtrair)
-const r2 = calcular(10)(5)(multiplicar)
-console.log(r1)
-console.log(r2)
+
+function sub(arg1,arg2){
+    return arg1 - arg2
+}
+
+
+function sum(arg1,arg2){
+    return arg1 + arg2
+}
+
+console.log(calc(mult))
+console.log(calc(div))
+console.log(calc(sub))
+console.log(calc(sum))
